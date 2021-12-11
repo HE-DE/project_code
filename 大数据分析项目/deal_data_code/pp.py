9e442ddcecd5f0a04938406a08f0cace4b3d97d9
HE-DE <201900130012@mail.sdu.edu.cn>
2021-12-11 09:37:53 +0800

    update

--- 大数据分析项目/deal_data_code/pp.py
+++ 大数据分析项目/deal_data_code/pp.py
@@ -1,5 +1,6 @@
 import os
 f=open("dir.txt","a")
-for root,dirs,files in os.walk("D:\\DA-code\\city\\xinjiang"):
+for root,dirs,files in os.walk("D:\\data_vis\\DATA_FOR_PROJECT\\country"):
     for file in files:
         f.writelines(os.path.join(root,file)+"\n")
+        
